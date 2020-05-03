c2bf5e4428fc2ce3bf71108739d12d1c4eb692e8
Siddharth Gupta <siddharthg20012@gmail.com>
2020-05-02 21:33:24 -0400

    Stops if no matches

--- studySearcher/main.py
+++ studySearcher/main.py
@@ -12,7 +12,6 @@
 #     return r.json()
 
 def auto_summarize(text):
-    # TODO: implement
     return text
 
 def best_studies(text, n = 5):
@@ -28,12 +27,12 @@ def best_studies(text, n = 5):
     for row, vals in enumerate(csv_reader[1:]):
         rankings.append([similarity_score(text, vals[1]), row])
     rankings.sort(key = lambda data: data[0])
-    tops = rankings[::-1][:5]
+    tops = rankings[::-1][:5]  # [[sim_score, row], x5]
     ret = []
     for i in tops:
+        if i[0] <= 0: break  # not accurate enough
         to_add = csv_reader[1 + i[1]]
         ret.append([to_add[0], auto_summarize(to_add[1]), to_add[2], to_add[3]])
-        # TODO: implement and integrate summarize function
     return ret
 
 if __name__ == "__main__":
